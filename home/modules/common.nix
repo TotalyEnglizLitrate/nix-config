803acd14b5f66943c0b622199e8b1dbeca171f73
Narendra S <narendra.s1232@gmail.com>
2025-02-08 21:38:28 +0530

    Add back neovim

--- home/modules/common.nix
+++ home/modules/common.nix
@@ -11,6 +11,7 @@
     ../modules/gpg.nix
     ../modules/home.nix
     ../modules/lazygit.nix
+    ../modules/neovim.nix
     ../modules/obs-studio.nix
     ../modules/packages.nix
     ../modules/scripts.nix
