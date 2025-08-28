75ea2e226445dd16b720cd4a8a0007b3d6c2c246
Narendra S <narendra.s1232@gmail.com>
2025-08-28 18:20:29 +0530

    Disable autologin

--- hosts/modules/gdm.nix
+++ hosts/modules/gdm.nix
@@ -1,9 +1,5 @@
 {...}: {
   services.displayManager.enable = true;
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "engliz";
-  };
   services.displayManager.gdm = {
     enable = true;
     wayland = true;
