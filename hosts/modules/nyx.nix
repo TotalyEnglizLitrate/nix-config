e7aa56b4fde603f66ced6e952f623a7fd2f49505
Narendra S <narendra.s1232@gmail.com>
2025-08-30 09:40:33 +0530

    Modify nyx config

--- hosts/modules/nyx.nix
+++ hosts/modules/nyx.nix
@@ -1,15 +1,20 @@
-{inputs, ...}: {
+{
+  inputs,
+  userConfig,
+  ...
+}: let
+  nixdir = "/home/${userConfig.name}/Documents/repositories/nix-config/";
+in {
   imports = [inputs.nyx.nixosModules.default];
   nyx = {
     enable = true;
     username = "engliz";
-    nixDirectory = "/home/engliz/Documents/repositories/nix-config/";
-    logDir = "/home/engliz/.cache/nyx/logs";
+    nixDirectory = nixdir;
+    logDir = "${nixdir}/.nyx-logs";
     autoPush = false;
 
     nyx-tool.enable = true;
     nyx-rebuild.enable = true;
     nyx-cleanup.enable = true;
-    nyx-tui.enable = true;
   };
 }
