cd1878be0331bdd687300198824899a6e2399c00
Narendra S <narendra.s1232@gmail.com>
2025-09-03 08:03:03 +0530

    Enable v4l2loopback module

--- hosts/modules/common.nix
+++ hosts/modules/common.nix
@@ -68,16 +68,16 @@
 
     # v4l (virtual camera) module settings
     kernelModules = [
-      # "v4l2loopback"
+      "v4l2loopback"
       "uinput"
       "hid-uclogic"
     ];
     extraModulePackages = with config.boot.kernelPackages; [
-      # v4l2loopback
+      v4l2loopback
     ];
-    # extraModprobeConfig = ''
-    #  options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-    # '';
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    '';
   };
 
   # Networking
