195c5e4c0c8772e7e6bb1e74aeef6259d4ac8000
Narendra S <narendra.s1232@gmail.com>
2025-09-12 19:10:56 +0530

    Theme attempt # 2

--- files/configs/nvim/lua/config/lazy.lua
+++ files/configs/nvim/lua/config/lazy.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
     lazy = false,
     version = false, -- always use the latest git commit
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = {},
   checker = {
     enabled = false,
     notify = false,
--- home/modules/stylix.nix
+++ home/modules/stylix.nix
@@ -1,18 +1,17 @@
 {pkgs, ...}: {
   stylix.enable = true;
   stylix.autoEnable = false;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/outrun-dark.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-terminal-dark.yaml";
   stylix.targets = {
-    foot.enable = true;
-    fish.enable = true;
-    starship.enable = true;
     bat.enable = true;
     btop.enable = true;
     lazygit.enable = true;
+    neovim.enable = true;
     swaync.enable = true;
     waybar.enable = true;
     qt.enable = true;
     gtk.enable = true;
+    tmux.enable = true;
     spicetify.enable = true;
   };
 }
