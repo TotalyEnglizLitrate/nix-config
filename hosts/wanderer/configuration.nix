1997ac4013408a9c42e36c70188a3d2df44eb72c
Narendra S <narendra.s1232@gmail.com>
2025-09-15 14:08:24 +0530

    Aggressively cache to avoid reading from thumbdrive as much as possible

--- hosts/wanderer/configuration.nix
+++ hosts/wanderer/configuration.nix
@@ -19,4 +19,11 @@
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   system.stateVersion = "24.11";
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 10;
+    "vm.vfs_cache_pressure" = 40;
+    "vm.dirty_background_ratio" = 5;
+    "vm.dirty_ratio" = 10;
+  };
 }
