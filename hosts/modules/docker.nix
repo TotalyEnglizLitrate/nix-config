b7ff1381d7e8c6e59f61c033dc366df9a651200c
Narendra S <narendra.s1232@gmail.com>
2025-10-07 19:07:56 +0530

    Switch docker storage driver to btrfs

--- hosts/modules/docker.nix
+++ hosts/modules/docker.nix
@@ -1,10 +1,14 @@
-{userConfig, ...}: {
+{ userConfig, ... }: {
   virtualisation.docker = {
     enable = false;
     rootless = {
       enable = true;
       setSocketVariable = true;
+      daemon.settings = {
+        storage-driver = "btrfs";
+      };
+
     };
   };
-  users.users.${userConfig.name}.extraGroups = ["docker" "video"];
+  users.users.${userConfig.name}.extraGroups = [ "docker" "video" ];
 }
