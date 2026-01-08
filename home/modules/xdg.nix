91764d0eb09a8e528803e39d60ba54bdd3631826
Narendra S <narendra.s1232@gmail.com>
2026-01-08 08:18:59 +0530

    xdg mime association

--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -5,25 +5,19 @@
       enable = true;
       associations.added = {
         "audio/mpeg" = ["vlc.desktop"];
-        "image/jpeg" = ["org.gnome.Loupe.desktop"];
-        "image/jpg" = ["org.gnome.Loupe.desktop"];
-        "image/png" = ["org.gnome.Loupe.desktop"];
-        "video/mp3" = ["vlc.desktop"];
-        "video/mp4" = ["vlc.desktop"];
-        "video/quicktime" = ["vlc.desktop"];
-        "video/webm" = ["vlc.desktop"];
+        "application/pdf" = ["okular.desktop"];
+        "image/*" = ["org.gnome.Loupe.desktop"];
+        "video/*" = ["vlc.desktop"];
+        "audio/*" = ["vlc.desktop"];
       };
       defaultApplications = {
         "application/json" = ["gnome-text-editor.desktop"];
         "application/toml" = "org.gnome.TextEditor.desktop";
         "application/x-gnome-saved-search" = ["org.gnome.Nautilus.desktop"];
+        "application/pdf" = ["okular.desktop"];
         "audio/*" = ["vlc.desktop"];
-        "audio/mp3" = ["vlc.desktop"];
         "image/*" = ["org.gnome.loupe.desktop"];
-        "image/jpg" = ["org.gnome.loupe.desktop"];
-        "image/png" = ["org.gnome.loupe.desktop"];
         "video/*" = ["vlc.desktop"];
-        "video/mp4" = ["vlc.desktop"];
       };
     };
     userDirs = {
