c96e988f12306c362080995d2b05315929630935
Narendra S <narendra.s1232@gmail.com>
2026-03-31 09:47:00 +0530

    nixos-cli: move into common module

--- hosts/modules/common.nix
+++ hosts/modules/common.nix
@@ -38,6 +38,18 @@
     };
   };
 
+  programs.nixos-cli = {
+    enable = true;
+    option-cache.enable = true;
+    settings = {
+      use_nvd = true;
+      apply = {
+        ignore_dirty_tree = true;
+        use_nom = true;
+      };
+    };
+  };
+
   environment = {
     etc =
       lib.mapAttrs' (name: value: {
