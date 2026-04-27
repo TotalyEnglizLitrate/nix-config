ff85ef62dbb8019d8a168c6c1974c4b66cc71dbd
Narendra S <narendra.s1232@gmail.com>
2026-04-27 18:44:13 +0530

    alias cd to z

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -43,13 +43,14 @@
       fish = {
         enable = true;
         shellAliases = {
+          cat = "bat";
+          cd = "z";
+          grep = "ugrep";
           ls = "eza --group-directories-first";
           dir = "eza --group-directories-first";
           ll = "eza -l --group-directories-first";
           la = "eza -lA --group-directories-first";
           tree = "eza --tree --group-directories-first";
-          cat = "bat";
-          grep = "ugrep";
           "..." = "../..";
         };
 
