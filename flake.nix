78afe535a3155c4662723364f525849b41ca1bae
Narendra S <narendra.s1232@gmail.com>
2026-04-29 17:14:39 +0530

    remove functions.nix

--- flake.nix
+++ flake.nix
@@ -62,9 +62,31 @@
       };
     };
 
-    functions = import ./functions.nix {inherit inputs outputs users;};
+    mkNixosConfiguration = hostname: username:
+     inputs.nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs outputs hostname;
+          userConfig = users.${username};
+        };
+        modules = [
+          ./hosts/${hostname}/configuration.nix
+          inputs.home-manager.nixosModules.home-manager
+          inputs.niri.nixosModules.niri
+          inputs.nixos-cli.nixosModules.nixos-cli
+          inputs.stylix.nixosModules.stylix
+          {
+            home-manager = {
+              extraSpecialArgs = {
+                inherit inputs outputs hostname;
+                userConfig = users.${username};
+              };
+              users.${username} = import ./home/${username}/${hostname}.nix;
+            };
+          }
+        ];
+    } ;
   in {
-    nixosConfigurations = with functions; {
+    nixosConfigurations = {
       latitude5491 = mkNixosConfiguration "latitude5491" "engliz";
       omnibook = mkNixosConfiguration "omnibook" "engliz";
       wanderer = mkNixosConfiguration "wanderer" "engliz";
