1a9028e4cd83f182febe2faff20640a8f3d1c2d1
Narendra S <narendra.s1232@gmail.com>
2026-05-29 09:47:33 +0530

    steam improvements

--- modules/steam/host.nix
+++ modules/steam/host.nix
@@ -1,6 +1,15 @@
-_: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
+
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamemode.enable = true;
+
+  environment = {
+    systemPackages = [pkgs.protonup-ng];
+    sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
   };
 }
