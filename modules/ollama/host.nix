2961c5554d989cb6a8e6c06326c2db18b0ca711c
Narendra S <narendra.s1232@gmail.com>
2026-06-25 23:28:33 +0530

    try llama.cpp instead of ollama

--- modules/ollama/host.nix
+++ modules/ollama/host.nix
@@ -3,12 +3,25 @@
   outputs,
   ...
 }: {
-  services.ollama = {
+  # services.ollama = {
+  #   enable = true;
+  #   rocmOverrideGfx = "11.5.1";
+  #   package = pkgs.ollama-rocm;
+  # };
+
+  services.llama-cpp = {
     enable = true;
-    rocmOverrideGfx = "11.5.1";
-    package = pkgs.ollama-rocm;
+    package = pkgs.llama-cpp-rocm;
   };
 
   nixpkgs.overlays = [outputs.overlays.toad];
-  environment.systemPackages = with pkgs; [goose-cli toad];
+  environment = {
+    systemPackages = with pkgs; [
+      goose-cli
+      toad
+      llama-cpp-rocm
+    ];
+
+    variables.HSA_OVERRIDE_GFX_VERSION="'11.5.0'";
+  };
 }
