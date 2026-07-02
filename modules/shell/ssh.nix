093f42e558ca57f7c15b6bfc63b2e87c30b81356
Narendra S <narendra.s1232@gmail.com>
2026-07-02 23:12:54 +0530

    ssh: matchBlocks -> settings

--- modules/shell/ssh.nix
+++ modules/shell/ssh.nix
@@ -4,18 +4,9 @@ _: {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."*" = {
-      forwardAgent = false;
+    settings."*" = {
       addKeysToAgent = "yes";
       identityFile = "~/.ssh/git";
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
     };
   };
 }
