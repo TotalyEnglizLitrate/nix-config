bc502f014180e078d043fdd9001d98eebd9f10ef
Narendra S <narendra.s1232@gmail.com>
2026-08-04 10:47:32 +0530

    chore(fmt)

--- modules/niri/noctalia/host.nix
+++ modules/niri/noctalia/host.nix
@@ -2,7 +2,7 @@
   imports = [inputs.noctalia-greetd.nixosModules.default];
   programs.noctalia-greeter.enable = true;
   nix.settings = {
-    substituters = [ "https://noctalia.cachix.org" ];
-    trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+    substituters = ["https://noctalia.cachix.org"];
+    trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
   };
 }
