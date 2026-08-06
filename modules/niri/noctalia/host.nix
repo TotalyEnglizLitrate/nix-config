fc5939bac7123398c284a953efa582d36595900e
Narendra S <narendra.s1232@gmail.com>
2026-08-06 13:11:12 +0530

    noctalia-greeter: appearance/cursor

--- modules/niri/noctalia/host.nix
+++ modules/niri/noctalia/host.nix
@@ -1,6 +1,17 @@
-{inputs, ...}: {
+{inputs, config, ...}: {
   imports = [inputs.noctalia-greetd.nixosModules.default];
-  programs.noctalia-greeter.enable = true;
+  programs.noctalia-greeter = {
+    enable = true;
+    settings = {
+      appearance.scheme = "Synced";
+      cursor = with config.stylix.cursor; {
+        theme = name;
+        inherit size;
+        path = "${package.outPath}/share/icons";
+      };
+    };
+  };
+
   nix.settings = {
     substituters = ["https://noctalia.cachix.org"];
     trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
