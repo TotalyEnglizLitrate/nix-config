dab0c5534b0bd284f04b6c75f317eeb0eff5c960
Narendra S <narendra.s1232@gmail.com>
2026-08-24 23:35:52 +0530

    gtk: make boot bookmark `admin://`

--- modules/desktop/common/gtk.nix
+++ modules/desktop/common/gtk.nix
@@ -5,22 +5,28 @@
   ...
 }: let
   isStandalone = osConfig == null;
+  contains = target: seq: (lib.lists.any (x: x == target) seq);
 in {
   gtk = {
     enable = true;
     gtk3 = {
       extraConfig.gtk-application-prefer-dark-theme = true;
 
-      bookmarks =
-        map (p: "file://${p}") (
-          map (p: "${config.home.homeDirectory}/${p}") [
-            "Documents"
-            "Downloads"
-            "Pictures"
-            "Videos"
-            "Documents/repos"
-          ]
-        ++ lib.optionals (!isStandalone) (builtins.attrNames osConfig.fileSystems));
+      bookmarks = let
+        needsAdmin = ["/boot"];
+      in (map (p:
+        if (contains p needsAdmin)
+        then "admin://${p}"
+        else "file://${p}") (
+        map (p: "${config.home.homeDirectory}/${p}") [
+          "Documents"
+          "Downloads"
+          "Pictures"
+          "Videos"
+          "Documents/repos"
+        ]
+        ++ lib.optionals (!isStandalone) (builtins.attrNames osConfig.fileSystems)
+      ));
     };
 
     gtk4.theme = lib.mkForce null;
